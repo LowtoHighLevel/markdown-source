3d1291f3d5270fedd889c97e1e7435d0285592ba
Cory Borek <agentdid127cb@aol.com>
2025-09-09 11:06:57 -0400

    clean up main.c

--- notes/asciitobin/main.c
+++ notes/asciitobin/main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 
 
+/**
+ * Converts a hex character to its numerical value.
+ */
 unsigned char hexToChar(char x) {
     if (x >= '0' && x <= '9') {
         return (unsigned char)(x - '0');
@@ -13,21 +16,32 @@ unsigned char hexToChar(char x) {
     }
     return 0;
 }
+
+/**
+ * Converts two ASCII characters to a binary byte
+ */
 unsigned char fromAscii(char x, char y) {
-return ((hexToChar(x) << 4) + hexToChar(y));
+    return ((hexToChar(x) << 4) + hexToChar(y));
 }
+
+/**
+ * Main program
+ */
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         return -1;
     }
 
+    // open output
     FILE* fptr;
     fptr = fopen(argv[1], "w");
 
     for (int i = 2; i< argc; i++) {
+        // convert hex byte to a unsigned integer
         if (strlen(argv[i]) == 2) {
             unsigned char f = fromAscii(argv[i][0], argv[i][1]);
 
+            // output binary
             fwrite(&f, sizeof(unsigned char), 1, fptr);
         }
     }
